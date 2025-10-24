4b1a091aa970939d54e43ef33c98310ea71b97ce
PascalGrenon <pgrenon@octogone.ai>
2025-10-24 13:45:57 -0400

    Revert "feat: create landing view with calculator modal for recipe widget"
    This reverts commit 48244c2f76ab471aa4c19f1c5fce9fbee05a9c6a.

--- src/components/widgets/octogone_recipe/index.ts
+++ src/components/widgets/octogone_recipe/index.ts
@@ -1,5 +1,4 @@
 export { OctogoneRecipeWidget } from './OctogoneRecipeWidget';
-export { OctogoneRecipeContainer } from './OctogoneRecipeContainer';
 export { RecipeIngredientsList } from './RecipeIngredientsList';
 export { RecipeSteps } from './RecipeSteps';
 export { ProductSideMenu } from './ProductSideMenu';
